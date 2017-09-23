805abd84d44b2718ee29a930332629eb3a2d4426
duncanmckee <duncan@mckee.net>
2017-09-23 15:07:09 -0700

    add teleop to control the soccerbots

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SoccerBot.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by Duncan on 9/23/2017.
+ */
+
+@TeleOp(name="Soccer Bot", group="Outreach")
+public class SoccerBot extends OpMode {
+
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+
+    @Override
+    public void init() {
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+    }
+
+    @Override
+    public void loop() {
+        leftDrive.setPower(Range.clip(-gamepad1.right_stick_y+gamepad1.right_stick_x,-1.0,1.0));
+        rightDrive.setPower(Range.clip(gamepad1.right_stick_y+gamepad1.right_stick_x,-1.0,1.0));
+    }
+}
