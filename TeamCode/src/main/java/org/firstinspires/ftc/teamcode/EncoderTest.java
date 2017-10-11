a895a9b9b82ca14a79e79f4a1a17880ae54efa2c
Ari Porad <ari@ariporad.com>
2017-10-11 16:23:42 -0700

    add encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+<<<<<<< HEAD
 import org.redshiftrobotics.lib.AutoOpMode;
 
 /**
@@ -10,11 +11,50 @@ public class EncoderTest extends AutoOpMode {
     @Override
     public void init() {
         super.init();
+=======
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.redshiftrobotics.lib.DcMotorEncoder;
+import org.redshiftrobotics.lib.EncoderPositioner;
+
+@TeleOp(name = "Encoder Test")
+public class EncoderTest extends OpMode {
+    EncoderPositioner positioner;
+
+    @Override
+    public void init() {
+        positioner = new EncoderPositioner(
+                new DcMotorEncoder(hardwareMap.dcMotor.get("x")),
+                new DcMotorEncoder(hardwareMap.dcMotor.get("y")),
+                ((7.125 * 2 * Math.PI) / 1300) // Not quite right
+        );
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        positioner.reset();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        positioner.reset();
+>>>>>>> 38dc098... add encoders
     }
 
     @Override
     public void loop() {
+<<<<<<< HEAD
         telemetry.addData("position", this.positioner.getPosition());
         telemetry.update();
     }
 }
+=======
+        telemetry.addData("Position", positioner.getPosition());
+        telemetry.update();
+    }
+}
+
+>>>>>>> 38dc098... add encoders
