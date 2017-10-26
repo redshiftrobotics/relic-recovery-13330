2e4e794d8430646b3d737b37f7f5ea339c44064a
duncanmckee <duncan@mckee.net>
2017-10-25 17:12:20 -0700

    update teleop to control mechanisms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Duncan on 9/19/2017.
@@ -14,6 +15,17 @@ public class MecanumTeleop extends OpMode{
     DcMotor backLeft;
     DcMotor backRight;
 
+    DcMotor conveyor;
+    Servo conveyorLift;
+
+    DcMotor leftIntake;
+    DcMotor rightIntake;
+
+    Servo leftJewel;
+    Servo rightJewel;
+
+
+
     @Override
     public void init() {
         frontLeft = hardwareMap.dcMotor.get("fl");
@@ -22,11 +34,32 @@ public class MecanumTeleop extends OpMode{
         backRight = hardwareMap.dcMotor.get("br");
         //frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         //backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        conveyor = hardwareMap.dcMotor.get("conveyor");
+        conveyorLift = hardwareMap.servo.get("conveyorLift");
+        leftIntake = hardwareMap.dcMotor.get("leftIntake");
+        rightIntake = hardwareMap.dcMotor.get("rightIntake");
+        leftJewel = hardwareMap.servo.get("leftJewel");
+        rightJewel = hardwareMap.servo.get("rightJewel");
     }
 
     @Override
     public void loop() {
+        controlConveyor();
+        controlJewel();
         drive();
+
+    }
+
+    private void controlConveyor(){
+        conveyor.setPower(gamepad1.a?1.0f:(gamepad1.b?-1.0f:0.0));
+        conveyorLift.setPosition(gamepad1.x?1.0f:(gamepad1.y?0.0f:conveyorLift.getPosition()));
+        leftIntake.setPower(gamepad1.right_bumper?-1.0f:(gamepad1.right_trigger));
+        rightIntake.setPower(gamepad1.right_bumper?-1.0f:(gamepad1.right_trigger));
+    }
+
+    private void controlJewel(){
+        leftJewel.setPosition(0.0);
+        rightJewel.setPosition(0.0);
     }
 
     private void drive(){
