d451133e2f4f22a16e10a25afb52b2f7d61258b0
Ari Porad <ari@ariporad.com>
2017-12-15 17:44:39 -0800

    Finish Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/PulsarTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/PulsarTeleop.java
@@ -14,12 +14,14 @@ import org.firstinspires.ftc.teamcode.lib.PulsarRobotHardware;
 public class PulsarTeleop extends LinearOpMode{
     private PulsarRobotHardware hw;
 
-    private static final double CONVEYOR_BELT_POWER_SCALAR = 0.85;
+    private static final double CONVEYOR_BELT_POWER_SCALAR = 0.6;
 
     private static final double INTAKE_POWER_SCALAR = 0.5;
     private static final double INTAKE_TURNING_SCALAR = 0.1;
 
     private static final double DRIVE_POWER_SCALAR = 0.95;
+
+    private static final double COLLECT_TIME = 3000;
 
     private double xPower = 0;
     private double yPower = 0;
@@ -32,6 +34,12 @@ public class PulsarTeleop extends LinearOpMode{
     private double pConstant = 5;
 
     private boolean isTurning = false;
+    private boolean lastRB = false;
+    private boolean lastLB = false;
+
+    private double collectStartTime = 0;
+    private boolean isCollecting = false;
+    private boolean lastBack = false;
 
     private double flPower = 0;
     private double frPower = 0;
@@ -54,6 +62,7 @@ public class PulsarTeleop extends LinearOpMode{
         hw.conveyorDown();
 
         telemetry.addLine("Ready for TeleOp!");
+        telemetry.addLine("IT WORKS");
         telemetry.addLine();
         telemetry.addLine("Press START to Begin!");
         telemetry.update();
@@ -62,15 +71,44 @@ public class PulsarTeleop extends LinearOpMode{
 
         while (opModeIsActive()) {
             readDriverControls(gamepad1);
+            fineTuneCurrentAngle(gamepad2);
             //debug_updatePConstant(gamepad2);
             computePLoop();
             driveMotors();
             controlIntake(gamepad2);
             controlConveyor(gamepad2);
+            controlIntakeShortcut(gamepad2);
             storeCryptoboxAlignment(gamepad2);
             sendTelemetry();
             idle();
         }
+    }
+
+    private void controlIntakeShortcut(Gamepad pad) {
+        if (pad.dpad_up != lastBack && pad.dpad_up && !isCollecting) {
+            telemetry.addLine("Collecting...");
+            isCollecting = true;
+            hw.intakeUp();
+            hw.conveyor.setPower(hw.CONVEYOR_SPEED);
+            collectStartTime = System.currentTimeMillis();
+        }
+
+        if (isCollecting && System.currentTimeMillis() - collectStartTime >= COLLECT_TIME) {
+            telemetry.addLine("Done Collecting");
+            hw.conveyor.setPower(0);
+            hw.intakeDown();
+            collectStartTime = 0;
+            isCollecting = false;
+        }
+
+        lastBack = pad.dpad_up;
+    }
+
+    private void fineTuneCurrentAngle(Gamepad pad) {
+        if (pad.dpad_right != lastRB && pad.dpad_right) { targetAngle += 5; }
+        if (pad.dpad_left != lastLB && pad.dpad_left) { targetAngle -= 5; }
+        lastRB = pad.dpad_right;
+        lastLB = pad.dpad_left;
     }
 
     private void storeCryptoboxAlignment(Gamepad pad) { //Function that updates the stored rotation, can be customized to use a specific controller
@@ -90,8 +128,8 @@ public class PulsarTeleop extends LinearOpMode{
         hw.rightIntake.setPower((-pad.right_stick_y * INTAKE_POWER_SCALAR) + (pad.right_stick_x * INTAKE_TURNING_SCALAR));
 
         // Intake Positioning
-        if (pad.a) { hw.intakeUp(); }
-        if (pad.y) { hw.intakeDown(); }
+        if (pad.y) { hw.intakeUp(); }
+        if (pad.a) { hw.intakeDown(); }
     }
 
     private void controlConveyor(Gamepad pad) {
@@ -99,8 +137,8 @@ public class PulsarTeleop extends LinearOpMode{
         hw.conveyor.setPower(-pad.left_stick_y * CONVEYOR_BELT_POWER_SCALAR);
 
         // Conveyor Positioning
-        if (pad.dpad_up) { hw.conveyorUp(); }
-        if (pad.dpad_down) { hw.conveyorDown(); }
+        //if (pad.dpad_up) { hw.conveyorUp(); }
+        //if (pad.dpad_down) { hw.conveyorDown(); }
     }
 
     private void driveMotors(){ //Function to control all hardware devices on the robot
