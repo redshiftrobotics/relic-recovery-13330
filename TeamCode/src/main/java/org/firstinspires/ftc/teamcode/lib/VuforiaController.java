c9834022348664be4d99f56d2dfbaa5bac2d0d32
Ari Porad <ari@ariporad.com>
2018-01-04 16:54:52 -0800

    chore: update file permissions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/VuforiaController.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.lib;
+
+/**
+ * Created by ariporad on 2017-12-16.
+ */
+
+public class VuforiaController {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TestVuforia.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by ariporad on 2017-12-16.
+ */
+
+public class TestVuforia extends OpMode {
+}
